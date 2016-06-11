1e8348b363e27d74b65c8f6ec00f8678185c87b9
Mansi <mpatel.y@gmail.com>
2016-06-11 13:06:56 -0400

    Added DepartmentGridView

--- lab3-departments/Departments.aspx
+++ lab3-departments/Departments.aspx
@@ -1,5 +1,20 @@
 ﻿<%@ Page Title="Departments" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Departments.aspx.cs" Inherits="lab3_departments.Departments" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8"></div>
+            <h1>Department List</h1>
+            <a href="DepartmentList.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus"></i>Add Department</a>
+            <asp:GridView runat="server" CssClass="table table-bordered table-stripped table-hover" ID="DepartmentsGridView" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="DepartmentID" HeaderText="Department ID" Visible="true" />
+                    <asp:BoundField DataField="Name" HeaderText="Department Name" Visible="true" />
+                    <asp:BoundField DataField="Budget" HeaderText="Department Budget" Visible="true"
+                        DataFormatString="{0:###,###.00}" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
 </asp:Content>
+
--- lab3-departments/Departments.aspx.cs
+++ lab3-departments/Departments.aspx.cs
@@ -5,13 +5,49 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//using statements are required to to connect to EF DB
+using lab3_departments.Models;
+using System.Web.ModelBinding;
+
 namespace lab3_departments
 {
     public partial class Departments : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //if loading the page for the first time populate the departements grid
+            if (!IsPostBack)
+            {
+                //get the departments data
+                this.GetDepartments();
+            }
         }
+
+        /***
+         * <summary>
+         * this method gets the departments data 
+         * </summary>
+         * 
+         * @method GetDepartments
+         * @return {void}
+         * **/
+
+            protected void GetDepartments()
+        {
+            //connect to EF
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //query the studnets table usinf EF and LINQ
+                var Departments = (from allDepartments in db.Departments
+                                   select allDepartments);
+
+                //bind the result in GridView
+                DepartmentsGridView.DataSource = Departments.ToList();
+                DepartmentsGridView.DataBind();
+
+                
+            }
+        }
+        
     }
 }
