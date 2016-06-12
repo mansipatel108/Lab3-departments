90d38f7ffee5331d84c31974f19a3142dce83662
Mansi <mpatel.y@gmail.com>
2016-06-11 20:56:13 -0400

    edit department working with pagination

--- lab3-departments/DepartmentDetails.aspx.cs
+++ lab3-departments/DepartmentDetails.aspx.cs
@@ -17,7 +17,32 @@ namespace lab3_departments
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetDepartment();
+            }
 
+        }
+
+        protected void GetDepartment()
+        {
+            //populated the form existing data from the database
+            int DepartmentID = Convert.ToInt32(Request.QueryString["DepartmentID"]);
+
+            //connect to the EF DB
+            using(DefaultConnection db = new DefaultConnection())
+            {
+                //populate a department object instance wit the DepartmentID from the url Parameter
+                Department updatedDepartment = (from department in db.Departments
+                                                where department.DepartmentID == DepartmentID
+                                                select department).FirstOrDefault();
+                //map the department properties to the form controls
+                if(updatedDepartment != null)
+                {
+                    DepartmentName.Text = updatedDepartment.Name;
+                    DepartmentBudget.Text = updatedDepartment.Budget.ToString();
+                }
+            }
         }
  
         protected void CancelButton_Click(object sender, EventArgs e)
@@ -34,14 +59,29 @@ namespace lab3_departments
                 //use the Department models to create a new deapartment object and 
                 //save a new record
                 Department newDepartment = new Department();
+
+                int DepartmentID = 0;
+
+                if(Request.QueryString.Count >0 ) //our URL has a DepartmentID in it 
+                {
+                    //get the id from the url
+                    DepartmentID = Convert.ToInt32(Request.QueryString["DepartmentID"]);
+
+                    //get the current department from EF DB
+                    newDepartment = (from department in db.Departments
+                                     where department.DepartmentID == DepartmentID
+                                     select department).FirstOrDefault();
+                }
+
                 //add data to the new department record
                 newDepartment.Name = DepartmentName.Text;
                 newDepartment.Budget = Convert.ToDecimal(DepartmentBudget.Text);
 
                 //use LINQ to ADO.NET to add/insert departments in to DB
+                if(DepartmentID == 0) { 
                 db.Departments.Add(newDepartment);
-
-                //save our changes
+                }
+                //save our changes also updates the DB
                 db.SaveChanges();
 
                 //redirect back to updated departments page
--- lab3-departments/Departments.aspx
+++ lab3-departments/Departments.aspx
@@ -10,10 +10,10 @@
             <div>
                 <label for="PageSizeDropDownList">Records per Page: </label>
                 <asp:DropDownList ID="PageSizeDropDownList" runat="server"
-                     AutoPostBack="true" CssClass="btn btn-default btn-sm dropdown-toggle" OnSelectedIndexChanged="PageSizeDropDownList_SelectedIndexChanged">
+                    AutoPostBack="true" CssClass="btn btn-default btn-sm dropdown-toggle" OnSelectedIndexChanged="PageSizeDropDownList_SelectedIndexChanged">
                     <asp:ListItem Text="3" Value="3" />
                     <asp:ListItem Text="5" Value="5" />
-                     <asp:ListItem Text="10" Value="10" />
+                    <asp:ListItem Text="10" Value="10" />
                     <asp:ListItem Text="All" Value="1000" />
                 </asp:DropDownList>
             </div>
@@ -21,12 +21,16 @@
             <asp:GridView runat="server" CssClass="table table-bordered table-stripped table-hover" ID="DepartmentsGridView" AutoGenerateColumns="false"
                 DataKeyNames="DepartmentID" OnRowDeleting="DepartmentsGridView_RowDeleting" AllowPaging="true" PageSize="3"
                 OnPageIndexChanging="DepartmentsGridView_PageIndexChanging"
-                 AllowSorting="true" OnSorting="DepartmentsGridView_Sorting" OnRowDataBound="DepartmentsGridView_RowDataBound">
+                AllowSorting="true" OnSorting="DepartmentsGridView_Sorting" OnRowDataBound="DepartmentsGridView_RowDataBound"
+                 PagerStyle-CssClass="pagination-ys">
                 <Columns>
                     <asp:BoundField DataField="DepartmentID" HeaderText="Department ID" Visible="true" SortExpression="DepartmentID" />
                     <asp:BoundField DataField="Name" HeaderText="Department Name" Visible="true" SortExpression="Name" />
                     <asp:BoundField DataField="Budget" HeaderText="Department Budget" Visible="true"
-                        DataFormatString="{0:###,###.00}"  SortExpression="Budget" />
+                        DataFormatString="{0:###,###.00}" SortExpression="Budget" />
+                    <asp:HyperLinkField HeaderText="Edit" Text="<i class='fa fa-pencil-square-o fa-lg'></i> Edit" 
+                        NavigateUrl="~/DepartmentDetails.aspx" ControlStyle-CssClass="btn btn-primary btn-sm" runat="server"
+                         DataNavigateUrlFields="DepartmentID" DataNavigateUrlFormatString="DepartmentDetails.aspx?DepartmentID={0}"  />
                     <asp:CommandField HeaderText="Delete" DeleteText="<i class='fa fa-trash-o fa-lg'></i> Delete" ShowDeleteButton="true" ButtonType="Link"
                         ControlStyle-CssClass="btn btn-danger btn-sm" />
                 </Columns>
